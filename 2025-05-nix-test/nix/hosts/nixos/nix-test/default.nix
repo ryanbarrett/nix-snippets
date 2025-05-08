3c3c7c42a1014998d2fc5444665fec7f718e7fd5
Ryan Barrett <barrettorama@gmail.com>
2025-05-07 19:38:50 -0500

    test

--- 2025-05-nix-test/nix/hosts/nixos/nix-test/default.nix
+++ 2025-05-nix-test/nix/hosts/nixos/nix-test/default.nix
@@ -13,6 +13,9 @@
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.useOSProber = true;
 
   # Network configuration
   networking = {
